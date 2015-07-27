dde3327f5f152e5f568f29bceae4d56926d207ff
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-28 01:07:49 +0300

    first update

---
+++ src/Anonym/Components/Session.php
@@ -0,0 +1,26 @@
+<?php
+    /**
+     * Bu Dosya AnonymFramework'e ait bir dosyadır.
+     *
+     * @author vahitserifsaglam <vahit.serif119@gmail.com>
+     * @see http://gemframework.com
+     *
+     */
+
+    namespace Anonym\Components\Session;
+
+    /**
+     * Class Session
+     * @package Anonym\Components\Session
+     */
+    class Session
+    {
+
+        /**
+         * Session ön ekini tutar
+         *
+         * @var string
+         */
+        private $prefix;
+
+    }

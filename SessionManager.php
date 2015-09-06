47b62648af074f177e29206813589413c2e6fd75
anonymframework <serif.saglam119@outlook.com>
2015-09-07 00:45:34 +0300

    made some updates

---
+++ SessionManager.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+
+namespace Anonym\Components\Session;
+
+/**
+ * Class SessionManager
+ * @package Anonym\Components\Session
+ */
+class SessionManager
+{
+
+    /**
+     * the list of session drivers
+     *
+     * @var array
+     */
+    protected $drivers;
+
+    /**
+     * the list of driver creator
+     *
+     * @var array
+     */
+    protected $creators;
+}

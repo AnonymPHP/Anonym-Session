d9216b479ea9477db647219dcdd3f4313970cd41
anonymframework <serif.saglam119@outlook.com>
2015-09-09 01:20:50 +0300

    made some updates

--- Stroge.php
+++ Stroge.php
@@ -10,10 +10,33 @@
 
 
 namespace Anonym\Components\Session;
-
+use SessionHandlerInterface;
 
 class Stroge
 {
 
+    /**
+     * the instance of handler
+     *
+     * @var SessionHandlerInterface
+     */
+    private $handler;
+
+    /**
+     * all configs of the handlers
+     *
+     * @var array
+     */
+    private $configs;
+
+    /**
+     * create a new instance and save handler settings
+     *
+     * @param array $configs all settings of the handler
+     * @param SessionHandlerInterface|null $handlerInterface the instance of  the handler
+     */
+    public function __construct(array $configs = [], SessionHandlerInterface $handlerInterface = null){
+
+    }
 
 }
--- index.php
+++ index.php
@@ -9,4 +9,3 @@
 
 include 'vendor/autoload.php';
 
-

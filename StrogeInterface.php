0d9dbc964ad195a471b4e9ae5997077c646cff22
anonymframework <serif.saglam119@outlook.com>
2015-09-09 01:25:25 +0300

    made some updates

--- StrogeInterface.php
+++ StrogeInterface.php
@@ -18,4 +18,37 @@ namespace Anonym\Components\Session;
 interface StrogeInterface
 {
 
+    /**
+     * return a registered session, return false on session not found
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function get($name);
+
+    /**
+     * register a new session with handler
+     *
+     * @param string $name the name of session
+     * @param mixed $value the value of session, the value can be string, object, integer ...
+     * @param int $time    if your driver support this, you can use it
+     * @return $this
+     */
+    public function set($name = '', $value, $time = 0);
+
+    /**
+     * remove a session
+     *
+     * @param string $name
+     * @return $this
+     */
+    public function delete($name);
+
+    /**
+     * clear the all sessions
+     *
+     * @return $this
+     */
+    public function flush();
+
 }
